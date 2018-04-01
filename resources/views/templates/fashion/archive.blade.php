b6cb9aad62a7569d69eaf00ff9eeac6900297f57
Nguyen Dinh Luyen <luyen.nguyen.itbb@jvn.edu.vn>
2018-04-01 23:09:02 +0700

    update resource fashion

---
+++ resources/views/templates/fashion/archive.blade.php
@@ -0,0 +1,182 @@
+@extends('templates.fashion.master')
+@section('title', 'Fashion: HTML5/CSS3 Template')
+@section('content')
+<section id="article-section" class="line archive">
+        <div class="margin">
+           <!-- ARTICLES -->
+           <div class="s-12 m-7 l-9">
+              <!-- ARTICLE 1 -->
+              <article class="margin-bottom">
+                 <div class="post-1 line">
+                    <!-- image -->
+                    <div class="s-12 l-11 post-image">
+                       <a href="post-1.html"><img src="/templates/fashion/img/post1-big.jpg" alt="Fashion"></a>
+                    </div>
+                    <!-- date -->
+                    <div class="s-12 l-1 post-date">
+                       <p class="date">07</p>
+                       <p class="month">mar</p>
+                    </div>
+                 </div>
+                 <!-- text -->
+                 <div class="post-text">
+                    <a href="post-1.html">
+                       <h2>Free responsive template</h2>
+                    </a>
+                    <p>
+                       Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                       aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+                       ex ea commodo consequat...
+                    </p>
+                    <a class="continue-reading" href="post-1.html">Continue reading</a>
+                 </div>
+              </article>
+
+              <!-- ARTICLE 2 -->
+              <article class="margin-bottom">
+                 <div class="post-2 line">
+                    <!-- image -->
+                    <div class="s-12 l-11 post-image">
+                       <a href="post-2.html"><img src="/templates/fashion/img/post2-big.jpg" alt="Fashion"></a>
+                    </div>
+                    <!-- date -->
+                    <div class="s-12 l-1 post-date">
+                       <p class="date">06</p>
+                       <p class="month">mar</p>
+                    </div>
+                 </div>
+                 <!-- text -->
+                 <div class="post-text">
+                    <a href="post-2.html">
+                       <h2>Amazing fashion blog</h2>
+                    </a>
+                    <p>
+                       Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                       aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+                       ex ea commodo consequat...
+                    </p>
+                    <a class="continue-reading" href="post-2.html">Continue reading</a>
+                 </div>
+              </article>
+
+              <!-- ARTICLE 3 -->
+              <article class="margin-bottom">
+                 <div class="post-3 line">
+                    <!-- image -->
+                    <div class="s-12 l-11 post-image">
+                       <a href="post-3.html"><img src="/templates/fashion/img/post3-big.jpg" alt="Fashion"></a>
+                    </div>
+                    <!-- date -->
+                    <div class="s-12 l-1 post-date">
+                       <p class="date">04</p>
+                       <p class="month">mar</p>
+                    </div>
+                 </div>
+                 <!-- text -->
+                 <div class="post-text">
+                    <a href="post-3.html">
+                       <h2>Nice minimalist design</h2>
+                    </a>
+                    <p>
+                       Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                       aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+                       ex ea commodo consequat...
+                    </p>
+                    <a class="continue-reading" href="post-3.html">Continue reading</a>
+                 </div>
+              </article>
+
+              <!-- ARTICLE 4 -->
+              <article class="margin-bottom">
+                 <div class="post-4 line">
+                    <!-- image -->
+                    <div class="s-12 l-11 post-image">
+                       <a href="post-4.html"><img src="/templates/fashion/img/post4-big.jpg" alt="Fashion"></a>
+                    </div>
+                    <!-- date -->
+                    <div class="s-12 l-1 post-date">
+                       <p class="date">01</p>
+                       <p class="month">mar</p>
+                    </div>
+                 </div>
+                 <!-- text -->
+                 <div class="post-text">
+                    <a href="post-4.html">
+                       <h2>With advertising regions</h2>
+                    </a>
+                    <p>
+                       Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                       aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+                       ex ea commodo consequat...
+                    </p>
+                    <a class="continue-reading" href="post-4.html">Continue reading</a>
+                 </div>
+              </article>
+
+              <!-- ARTICLE 5 -->
+              <article class="margin-bottom">
+                 <div class="post-5 line">
+                    <!-- date -->
+                    <div class="s-12 post-date">
+                       <p class="date">27</p>
+                       <p class="month">feb</p>
+                    </div>
+                 </div>
+                 <!-- text -->
+                 <div class="post-text">
+                    <a href="post-5.html">
+                       <h2>And again - it's totally free!</h2>
+                    </a>
+                    <p>
+                       Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                       aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+                       ex ea commodo consequat...
+                    </p>
+                    <a class="continue-reading" href="post-5.html">Continue reading</a>
+                 </div>
+              </article>
+           </div>
+           <!-- SIDEBAR -->
+           <div class="s-12 m-5 l-3">
+              <aside>
+                 <!-- NEWS 1 -->
+                 <img src="/templates/fashion/img/news1.jpg" alt="News 1">
+                 <div class="aside-block margin-bottom">
+                    <h3>This is the first news on blog</h3>
+                    <p>Lorem ipsum dolor sit amet, conse ctetuer. Duis autem vemeu iriure dolor adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam</p>
+                 </div>
+                 <!-- AD REGION -->
+                 <div class="advertising margin-bottom hide-s">
+                   <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                   <!-- Fashion 160x600 -->
+                   <ins class="adsbygoogle"
+                        style="display:inline-block;width:160px;height:600px"
+                        data-ad-client="ca-pub-8115128083480193"
+                        data-ad-slot="7468355662"></ins>
+                   <script>
+                   (adsbygoogle = window.adsbygoogle || []).push({});
+                   </script>
+                 </div>
+                 <!-- NEWS 2 -->
+                 <img src="/templates/fashion/img/news2.jpg" alt="News 2">
+                 <div class="aside-block margin-bottom">
+                    <h3>Second news on the fashion blog</h3>
+                    <p>Lorem ipsum dolor sit amet, conse ctetuer. Duis autem vemeu iriure dolor adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam</p>
+                 </div>
+                 <!-- AD REGION -->
+                 <div class="advertising margin-bottom hide-s">
+                   <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                   <!-- Fashion 160x600 -->
+                   <ins class="adsbygoogle"
+                        style="display:inline-block;width:160px;height:600px"
+                        data-ad-client="ca-pub-8115128083480193"
+                        data-ad-slot="7468355662"></ins>
+                   <script>
+                   (adsbygoogle = window.adsbygoogle || []).push({});
+                   </script>
+                 </div>
+              </aside>
+           </div>
+        </div>
+     </section>
+@endsection
---
+++ resources/views/templates/fashion/header.blade.php
@@ -0,0 +1,34 @@
+<header class="margin-bottom">
+    <div class="line">
+        <nav>
+            <div class="top-nav">
+            <p class="nav-text"></p>
+            <a class="logo" href="index.html">
+            Fashion<span>Blog</span>
+            </a>
+            <ul class="top-ul right">
+                <li>
+                    <a href="{{ route('temp.fashion.index') }}">Blog</a>
+                </li>
+                <li>
+                    <a href="{{ route('temp.fashion.about') }}">About</a>
+                </li>
+                <li>
+                    <a href="{{ route('temp.fashion.archive') }}">Archive</a>
+                </li>
+                <li>
+                    <a href="{{ route('temp.fashion.contact') }}">Contact</a>
+                </li>
+                <div class="social right">
+                    <a target="_blank" href="">
+                    <i class="icon-facebook_circle icon2x"></i>
+                    </a>
+                    <a target="_blank" href="">
+                    <i class="icon-twitter_circle icon2x"></i>
+                    </a>
+                </div>
+            </ul>
+            </div>
+        </nav>
+    </div>
+</header>
